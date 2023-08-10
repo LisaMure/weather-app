9e638ce2e9b626bffdc2f0ef65248dcedb057e60
LisaMure <140246111+LisaMure@users.noreply.github.com>
2023-08-10 21:41:16 +0200

    Added javascript to add date for Last update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayTemperature(response) {
   let mainTemperature = document.querySelector("#main-temp");
   mainTemperature.innerHTML = Math.round(response.data.main.temp);
@@ -5,7 +37,6 @@ function displayTemperature(response) {
   minimumTemperature.innerHTML = Math.round(response.data.main.temp_min);
   let maximumTemperature = document.querySelector("#max-temp");
   maximumTemperature.innerHTML = Math.round(response.data.main.temp_max);
-  console.log(response.data);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
   let weatherDescription = document.querySelector("#weather-description");
@@ -15,7 +46,7 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind-speed");
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#current-date");
-  //dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "8b058902e56924ef72769023553cf986";
